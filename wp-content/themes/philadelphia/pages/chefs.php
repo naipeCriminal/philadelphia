e66a8888c947e25d1629488749763a853b693666
elflaco <redasterix89@gmail.com>
2015-04-14 17:52:49 -0500

    chefs

--- wp-content/themes/philadelphia/pages/chefs.php
+++ wp-content/themes/philadelphia/pages/chefs.php
@@ -1,215 +1,201 @@
-<div class="container chefs">
-<div class="row">
-<div class="col-md-12">
-<h1><span style="color: #1a477e; text-align: center; font-size: 35px;"> centro gastronómico mondeléz</span></h1>
-</div>
-<div class="col-xs-12 col-md-6 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-rodrigo.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<div class="descripcionChef" style="background: #0c4ca3;">
-<a href="#rodrigoZepeda" data-toggle="modal"><h1>RODRIGO ZEPEDA</h1></a>
-<h3>Corporative Chef</h3>
-</div>
-</div>
-</div>
-<div class="col-xs-12 col-md-6 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-karlaferrer.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<div class="descripcionChef" style="background: #0097fe;">
-<a href="#karlaF" data-toggle="modal"><h1>KARLA FERRER</h1></a>
-<h3>Chef Jr.</h3>
-</div>
-</div>
-</div>
-<div class="col-xs-12 col-md-6 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-marcos.jpg');">
-<div class="pleca">
-<div class="descripcionChef" style="background: #003a60;">
-<a href="#marcosV" data-toggle="modal"><h1>MARCOS VALADEZ</h1></a>
-<h3>Chef Jr.</h3>
-</div>
-</div>
-</div>
-<div class="col-xs-12 col-md-6 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-astrid.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<div class="descripcionChef" style="background: #00adef;">
-<a href="#astridB" data-toggle="modal"><h1>ASTRID BENÍTEZ</h1></a>
-<h3>Chef Jr.</h3>
-</div>
-</div>
-</div>
-<div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-quees.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<h1 class="bemio">¿QUÉ ES EL
-<span class="bjack">Centro Gastronomico?</span></h1>
-</div>
-</div>
-<div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-ubicacion.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<h1 class="bemio">UBICACIÓN</h1>
-</div>
-</div>
-<div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg'); background-size: cover; background-position: center center;">
-<div class="pleca">
-<h1 class="bemio"><a href="?page_id=469">NOTICIAS</a></h1>
-</div>
-</div>
-</div>
-</div>
-
-
-
-    <!-- Modal Contents -->
- <div id="rodrigoZepeda" class="modal fade ">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-           <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-             <h4 class="modal-title">Browser Update</h4>
-           </div>
-           <div class="modal-body">
-
-            <div class="row">
-              <div class="col-md-6">
-                <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-rodrigoo.jpg" alt="">
-              </div>
-              <div class="col-md-6">
-              <h1 class="bemio">RODRIGO ZEPEDA</h1>
-              <h3 class="bjack">Chef Jr.</h3>
-              <p></p>
-              <h2 class="bjack">Educación</h2>
-              <ul>
-                <li>Licenciatura en Gastronomía en el Centro de Estudios Superiores de San Ángel (Distrito Federal, México)</li>
-                <li>Escola de Cuina Terra d´Escue</li>
-                <li></li>
-                <li></li>
-              </ul>
-              <h2 class="bjack">Iniciis</h2>
-              <p></p>
-              <h2 class="bjack">Habilidades</h2>
-              <p></p>
-              <h2 class="bjack">Logros Notables</h2>
-              <p></p>
-              <h2 class="bjack">Inspiración para el trabajo</h2>
-              <p></p>     
-              </div>  
-            </div>
-
-
-           </div>
-        </div>
+<div class="safe-container chefs">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1><span style="color: #1a477e; font-size: 35px;">Centro Gastronómico Mondeléz</span></h1>
     </div>
-</div>
-
-    
-    <!-- Modal Contents -->
- <div id="karlaF" class="modal fade ">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-           <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-             <h4 class="modal-title">Browser Update</h4>
-           </div>
-           <div class="modal-body">
-
-            <div class="row">
-              <div class="col-md-6">
-                <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-karla.jpg" alt="">
-              </div>
-              <div class="col-md-6">
-              <h1 class="bemio">KARLA FERRER</h1>
-              <h3 class="bjack">Chef Jr.</h3>
-              <p></p>
-              <h2 class="bjack">Educación</h2>
-              <p></p>
-              <h2 class="bjack">Iniciis</h2>
-              <p></p>
-              <h2 class="bjack">Habilidades</h2>
-              <p></p>
-              <h2 class="bjack">Logros Notables</h2>
-              <p></p>
-              <h2 class="bjack">Inspiración para el trabajo</h2>
-              <p></p>     
-              </div>  
-            </div>
-
-
-           </div>
+    <div class="col-xs-12 col-md-6 seccion chef-grid" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-rodrigo.jpg'); background-size: cover; background-position: center center;">
+      <a href="#rodrigoZepeda" data-toggle="modal">
+        <div class="pleca">
+          <div class="descripcionChef">
+            <h1>RODRIGO ZEPEDA</h1>
+            <h3>Corporative Chef</h3>
+          </div>
         </div>
+      </a>
     </div>
-</div>
-
-
-    
-    <!-- Modal Contents -->
- <div id="marcosV" class="modal fade ">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-           <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-             <h4 class="modal-title">Browser Update</h4>
-           </div>
-           <div class="modal-body">
-
-            <div class="row">
-              <div class="col-md-6">
-                <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-marcoss.jpg" alt="">
-              </div>
-              <div class="col-md-6">
-              <h1 class="bemio">MARCOS VALADEZ</h1>
-              <h3 class="bjack">Chef Jr.</h3>
-              <p></p>
-              <h2 class="bjack">Educación</h2>
-              <p></p>
-              <h2 class="bjack">Iniciis</h2>
-              <p></p>
-              <h2 class="bjack">Habilidades</h2>
-              <p></p>
-              <h2 class="bjack">Logros Notables</h2>
-              <p></p>
-              <h2 class="bjack">Inspiración para el trabajo</h2>
-              <p></p>     
-              </div>  
-            </div>
-
-
-           </div>
+    <div class="col-xs-12 col-md-6 seccion chef-grid" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-karlaferrer.jpg'); background-size: cover; background-position: center center;">
+      <a href="#karlaF" data-toggle="modal">
+        <div class="pleca">
+          <div class="descripcionChef">
+            <h1>KARLA FERRER</h1>
+            <h3>Chef Jr.</h3>
+          </div>
         </div>
+      </a>
     </div>
-</div>
-
-    
-    <!-- Modal Contents -->
- <div id="astridB" class="modal fade ">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-           <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-             <h4 class="modal-title">Browser Update</h4>
-           </div>
-           <div class="modal-body">
-
-            <div class="row">
-              <div class="col-md-6">
-                <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-astridd.jpg" alt="">
-              </div>
-              <div class="col-md-6">
-              <h1 class="bemio">ASTRID BENÍTEZ</h1>
-              <h3 class="bjack">Chef Jr.</h3>
-              <p></p>
-              <h2 class="bjack">Educación</h2>
-              <p></p>
-              <h2 class="bjack">Iniciis</h2>
-              <p></p>
-              <h2 class="bjack">Habilidades</h2>
-              <p></p>
-              <h2 class="bjack">Logros Notables</h2>
-              <p></p>
-              <h2 class="bjack">Inspiración para el trabajo</h2>
-              <p></p>     
-              </div>  
-            </div>
-
-
-           </div>
+    <div class="col-xs-12 col-md-6 seccion chef-grid" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-marcos.jpg');">
+      <a href="#marcosV" data-toggle="modal">
+        <div class="pleca">
+          <div class="descripcionChef">
+            <h1>MARCOS VALADEZ</h1>
+            <h3>Chef Jr.</h3>
+          </div>
         </div>
+      </a>
     </div>
+    <div class="col-xs-12 col-md-6 seccion chef-grid" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-astrid.jpg'); background-size: cover; background-position: center center;">
+      <a href="#astridB" data-toggle="modal">
+        <div class="pleca">
+          <div class="descripcionChef">
+            <h1>ASTRID BENÍTEZ</h1>
+            <h3>Chef Jr.</h3>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-quees.jpg'); background-size: cover; background-position: center center;">
+      <div class="pleca">
+        <h1 class="bemio">¿QUÉ ES EL
+        <span class="bjack">Centro Gastronomico?</span></h1>
+      </div>
+    </div>
+    <div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-ubicacion.jpg'); background-size: cover; background-position: center center;">
+      <div class="pleca">
+        <h1 class="bemio">UBICACIÓN</h1>
+      </div>
+    </div>
+    <div class="col-xs-12 col-md-4 seccion" style="background: url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg'); background-size: cover; background-position: center center;">
+      <div class="pleca">
+        <h1 class="bemio"><a href="?page_id=469">NOTICIAS</a></h1>
+      </div>
+    </div>
+  </div>
 </div>
-
+<!-- Modal Contents -->
+<div id="rodrigoZepeda" class="modal fade ">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-body chef-modal-content">
+        <button type="button" class="close close-chef" data-dismiss="modal" aria-hidden="true">X</button>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-rodrigoo.jpg" alt="">
+          </div>
+          <div class="col-md-6">
+            <h1 class="bemio">RODRIGO ZEPEDA</h1>
+            <h3 class="bjack">Corporative Chef</h3>
+            <p>Con más de 10 años de trayectoria, Rodrigo está a cargo del desarrollo de recetas para el catálogo de Food Service, así como de la innovación de nuevos productos con base en las especificaciones de la empresa y los clientes. Imparte asesorías gastronómicas de producción y operación a clientes potenciales.</p>
+            <h2 class="bjack">Educación</h2>
+            <ul>
+              <li>Licenciatura en Gastronomía en el Centro de Estudios Superiores de San ángel (Distrito Federal, México)</li>
+              <li>Escola De Cuina Terra d’Escudella (Barcelona, España)</li>
+              <li>Escola de Cuina Arnadí (Barcelona, España)</li>
+              <li>Le Cordon Bleu (Paris, Francia)</li>
+            </ul>
+            <h2 class="bjack">Inicios</h2>
+            <p>Comenzó su trabajo en el restaurante Los Irabien, en México, a cargo del equipo de cocineros. Fue responsable de la implementación de nuevas recetas, así como la coordinación de eventos fuera del restaurante.</p>
+            <h2 class="bjack">Habilidades</h2>
+            <ul>
+              <li>Liderazgo</li>
+              <li>Creatividad</li>
+              <li>Conocimiento de la marca para la creación de desarrollos rentables</li>
+            </ul>
+            <h2 class="bjack">Logros Notables</h2>
+            <p>Desarrollo e implementación del catálogo exclusivo de recetas para el canal de Food Service.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal Contents -->
+<div id="karlaF" class="modal fade ">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-body chef-modal-content">
+        <button type="button" class="close close-chef" data-dismiss="modal" aria-hidden="true">X</button>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-karla.jpg" alt="">
+          </div>
+          <div class="col-md-6">
+            <h1 class="bemio">KARLA FERRER</h1>
+            <h3 class="bjack">Chef Jr.</h3>
+            <p>Con 9 años de experiencia, Karla es responsable de la plataforma digital de <b>Philadelphia Food Service</b> al estar a cargo de la elaboración de recetas nuevas, estandarización de las mismas, coordinación de clases de cocina, líder del call center que dá soluciones al consumifor, así como asesoría culinaria para los proyectos de marketing.</p>
+            <p></p>
+            <h2 class="bjack">Educación</h2>
+            <p>Licenciatura en Gastronomía en  el Colegio Superior de Gastronomía (Distrito Federal, México)</p>
+            <h2 class="bjack">Inicios</h2>
+            <p>Comenzó su trabajo en el Restaurante Bice Bistrod del Hotel Camino Real, creando y elaborando platillos de la carta, del área de desayunos y responsable de cocina fría.</p>
+            <h2 class="bjack">Logros Notables</h2>
+            <ul>
+              <li>Generación del contenido digital de recetas y clases de cocina de Philadelphia.</li>
+              <li>Con el apoyo de todo el equipo, posicionar la página de retail de Philadelphia como la #1 en FanPages de México.</li>
+            </ul>
+            <h2 class="bjack">Inspiración para el trabajo</h2>
+            <p>Karla cree que la cocina implica pasión en el trabajo y ganas de reinventar experiencias.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal Contents -->
+<div id="marcosV" class="modal fade ">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-body chef-modal-content">
+        <button type="button" class="close close-chef" data-dismiss="modal" aria-hidden="true">X</button>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-marcoss.jpg" alt="">
+          </div>
+          <div class="col-md-6">
+            <h1 class="bemio">MARCOS VALADEZ</h1>
+            <h3 class="bjack">Chef Jr.</h3>
+            <p>Con 16 años de experiencia, Marcos es responsable del desarrollo de recetas para nuevos clientes. Asimismo, imparte capacitación culinaria, especializándose en panadería y pastelería.</p>
+            <h2 class="bjack">Educación</h2>
+            <p>Diplomado en la Universidad del Claustro de Sor Juana (Distrito Federal, México)</p>
+            <h2 class="bjack">Inicios</h2>
+            <p>Comenzó su trabajo en la cadena de autoservicios Chedraui, realizando panadería francesa, bizcochos y pastelería.</p>
+            <h2 class="bjack">Habilidades</h2>
+            <p>Especialista en panadería y pastelería.</p>
+            <h2 class="bjack">Logros Notables</h2>
+            <p>Iniciar dentro de la empresa una fuerza de técnicos en panadería para la capacitación de nuevos productos y desarrollos.</p>
+            <h2 class="bjack">Inspiración para el trabajo</h2>
+            <ul>
+              <li>Capacitar e inspirar a las personas que lo rodean.</li>
+              <li>Otorgar recomendaciones culinarias, dando como resultado un gran platillo. Transformar una idea en un delicioso postre.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal Contents -->
+<div id="astridB" class="modal fade ">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-body chef-modal-content">
+        <button type="button" class="close close-chef" data-dismiss="modal" aria-hidden="true">X</button>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-astridd.jpg" alt="">
+          </div>
+          <div class="col-md-6">
+            <h1 class="bemio">ASTRID BENÍTEZ</h1>
+            <h3 class="bjack">Chef Jr.</h3>
+            <p>Con 5 años de experiencia, Astrid es responsable del desarrollo de nuevas recetas, a través de demostraciones con nuestros clientes. Brinda atención a los consumidores en distintas exposiciones, apoya con la estandarización de recetas y tiene a cargo el contenido de la plataforma del canal Food Service.</p>
+            <h2 class="bjack">Educación</h2>
+            <p>Licenciatura en Gastronomía en la Universidad del Claustro de Sor Juana (Distrito Federal, México).</p>
+            <h2 class="bjack">Inicios</h2>
+            <p>Comenzó su trabajo en el Hotel Iberostar en Playa del Carmen, estando a cargo del área de cocina caliente para buffet y restaurantes de especialización.</p>
+            <h2 class="bjack">Habilidades</h2>
+            <p>Mostrar la creatividad a través de la creación de diferentes platillos.</p>
+            <h2 class="bjack">Logros Notables</h2>
+            <ul>
+              <li>Obtener el rango de Sous Chef en el restaurante escuela “Zéfiro” logrando una destacada participación.</li>
+              <li>Obtener el primer lugar en desarrollo de productos cárnicos para una prestigiosa empresa nacional enfocada en dicho sector.</li>
+            </ul>
+            <h2 class="bjack">Inspiración para el trabajo</h2>
+            <ul>
+              <li>Dominar los pequeños detalles para lograr siempre excelentes resultados sea cual sea la actividad a realizar.</li>
+              <li>Disciplina y compromiso dentro de todas las áreas de gastronomía en la creación de nuevos platillos.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
