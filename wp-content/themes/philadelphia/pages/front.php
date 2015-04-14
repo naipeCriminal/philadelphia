f47c94d14bfd54183272298ca31b789f17c1af5a
elflaco <redasterix89@gmail.com>
2015-04-13 19:13:46 -0500

    header, menu, footer, index de recetas & tips

--- wp-content/themes/philadelphia/pages/front.php
+++ wp-content/themes/philadelphia/pages/front.php
@@ -3,22 +3,22 @@
         <div class="col-md-12">
           <h1>FOOD SERVICE: ¡DALE MÁS SABOR A TU NEGOCIO!</h1>
         </div>
-        <div class="col-xs-12 col-md-4 seccion" style="display:table;background:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-centrogastronomico.jpg)">
+        <div class="col-xs-12 col-md-4 seccion" style="display:table;background-image:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-centrogastronomico.jpg)">
           <div class="pleca" style="display:table-cell">
             <a href="?page_id=293"><h1 class="bemio">centro <br><span class="bjack">Gastronómico</span></h1></a>
           </div>          
         </div>
-        <div class="col-xs-12 col-md-8 seccion" style="display:table;background:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-recetastips.jpg)">
+        <div class="col-xs-12 col-md-8 seccion" style="display:table;background-image:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-recetastips.jpg)">
           <div class="pleca" style="display:table-cell">
             <a href="?page_id=381"><h1 class="bemio">recetas <br><span class="bjack">y Tips</span></h1></a>
           </div>          
         </div>
-        <div class="col-xs-12 col-md-8 seccion" style="display:table;background:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-pic.jpg)">
+        <div class="col-xs-12 col-md-8 seccion" style="display:table;background-image:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-pic.jpg)">
           <div class="pleca" style="display:table-cell">
             <a href="?page_id=447"><h1 class="bemio">professional <br><span class="bjack">Innovation Center</span> </h1></a>
           </div>          
         </div>
-        <div class="col-xs-12 col-md-4 seccion" style="display:table;background:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-casosexito.jpg)">
+        <div class="col-xs-12 col-md-4 seccion" style="display:table;background-image:url(<?php bloginfo('template_url'); ?>/assets/img/homefinal-casosexito.jpg)">
           <div class="pleca" style="display:table-cell">
             <a href="?page_id=317"><h1 class="bemio">casos <br><span class="bjack"> de éxito</span></h1></a>
           </div>          
