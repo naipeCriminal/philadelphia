2f6f35a2275ca01468b3a74b804d932ba3a2911f
elflaco <redasterix89@gmail.com>
2015-04-15 00:26:42 -0500

    noticias

--- wp-content/themes/philadelphia/pages/noticias.php
+++ wp-content/themes/philadelphia/pages/noticias.php
@@ -1,17 +1,26 @@
 <div class="safe-container">
+  <div class="row text-center">
+    <h1 class="uppercase bemio blue-default">Noticias</h1>
+  </div>
+</div>
+<div class="safe-container">
   <div class="row pic">
-    <h1 class="bemio" style="color: #1a477e;text-align: center;font-size: 35px;">NOTICIAS</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium reiciendis minus quaerat veniam alias rem consequatur repellat dolore odio accusamus, nisi officiis, maxime enim, hic expedita. Dolorum quos nemo veritatis.</p>
     <?php
     $args = array('showposts'=>-1,  'post_type'=>'noticia');
     $query = new WP_Query($args);
     if ( have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
     <?php global $post;$thumbID = get_post_thumbnail_id( $post->ID );$imgDestacada = wp_get_attachment_url( $thumbID );?>
-    <div class="col-xs-12 col-md-4 seccion" style="background-image:url(<?php echo $imgDestacada; ?>);">
+    <div class="col-md-4 seccion section-new" style="background-image:url(<?php echo $imgDestacada; ?>);">
       <div class="table">
         <div class="cell-bottom">
           <div class="pleca">
-            <h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+            <div class="table">
+              <div class="cell-center">
+                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="bemio">
+                  <h3 class="new-title"><?php the_title(); ?></h3>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- wp-content/themes/philadelphia/pages/queEs.php
+++ wp-content/themes/philadelphia/pages/queEs.php
@@ -1,36 +1,69 @@
 <div class="safe-container">
+  <div class="col-md-12 text-center">
+    <h1 class="bemio">CENTRO GASTRONÓMICO MONDELEZ</h1>
+  </div>
   <div class="row queEs">
-    <div class="col-xs-12 col-md-8 seccion" style="background:#00adef;">
+    <div class="col-xs-12 col-md-8 seccion text-center" style="background:#00adef;">
       <h1 class="bemio">¿Qué es el <br><span class="bjack">centro gastronómico?</span></h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sunt facere, hic animi. Pariatur, dicta omnis aspernatur quasi dolor. Tempora odit veniam possimus ad impedit distinctio quam nemo, dolorem iusto.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, consequuntur consectetur expedita tempora eius numquam, quaerat vitae vel deserunt? Nemo et fuga enim tempora cupiditate officia? Deleniti natus hic ex.</p>
-      <p><span class="bjack">¡Dale más sabor a tu negocio con PHILADELPHIA!</span></p>
+      <p class="text-left">En Mondelēz nos preocupamos por el crecimiento y la competitividad del mercado de Food Service. Es por eso que desarrollamos el Centro Gastronómico Mondelēz.</p>
+      <p class="text-left">Nuestro Centro Gastronómico cuenta con un equipo profesional, dedicado a combinar sabores, colores, olores y texturas para lograr creaciones culinatias con Queso Crema Philadelphia.</p>
+      <p class="text-left">Uno de nuestros objetivos principales es potencializar tu negocio; sin importar giro o tamaño, te apoyamos con la generación de ideas culinarias innovadoras basadas en las estaciones o tendencias, y te brindamos capacitación de procesos específicos durante todo el año.</p>
+      <h2><span class="bjack">¡Dale más sabor a tu negocio con Philadelphia!</span></h2>
     </div>
-    <div class="col-xs-12 col-md-4 seccion" style="background:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');background-size: cover; background-position: center center;">
+    <div class="col-xs-12 col-md-4 seccion" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');">
       <br>
     </div>
-    <div class="col-xs-12 col-md-6 seccion" style="background:#003a60;">
+    <div class="col-xs-12 col-md-6 seccion text-center" style="background:#003a60;">
       <h1 class="bemio">¿que hacemos <br> para tu negocio?</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et, reprehenderit dolorum sapiente tenetur. Eveniet sapiente laboriosam autem tenetur saepe maiores minima eligendi ea ad quis facere et, eius modi?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint impedit error atque voluptatibus accusamus, nam ex quas aperiam aut nulla cupiditate necessitatibus porro nihil vel molestias velit facilis id autem.</p>
-      <h1 class="bjack" style="font-size:24px">¡Ha tu negocio más versátil! <br>¡Personaliza tus recetas con Philadelphia!</h1>
+      <p class="text-left">Brindamos soporte en el desarrollo de recetas exclusivas de acuerdo a las necesidades de tu negocio, con la calidad y respaldo del gran reconocimiento de Philadelphia.
+        Te ofrecemos nuestras instalaciones para tener sesiones exclusivas de degustación con las recetas desarrolladas para tu negocio.
+        Damos capacitación en punto de venta sobre las recetas que creamos para ti.
+        Dale play al video y conoce más sobre nuestro Centro Gastronómico.</p>
+      <h1 class="bjack" style="font-size:24px">¡Haz tu negocio más versátil! <br>¡Personaliza tus recetas con Philadelphia!</h1>
+      <p>Escríbenos a: contactofoodservice@mdlz.com</p>
     </div>
     <div class="col-xs-12 col-md-6 seccion" style="padding:0px;">
       <iframe width="100%" height="350" src="https://www.youtube.com/embed/bTtHL4W8G68" frameborder="0" allowfullscreen></iframe>
     </div>
-    <div class="col-xs-12 col-md-4 seccion sinPadding" style="background:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-ubicacion.jpg');background-size: cover; background-position: center center;">
-      <div class="pleca">
-        <a href="?page_id=304"><h1 class="bemio">UBICACIÓN</h1></a>
+    <div class="col-xs-12 col-md-4 seccion" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-ubicacion.jpg');">
+      <div class="table">
+        <div class="cell-center">
+          <a href="?page_id=304">
+            <h1 class="bemio">UBICACIÓN</h1>
+          </a>
+        </div>
       </div>
     </div>
-    <div class="col-xs-12 col-md-4 seccion sinPadding" style="background:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-nuestroschefs.jpg');background-size: cover; background-position: center center;">
-      <div class="pleca conPleca">
-        <a href="?page_id=299"><h1 class="bemio"><span class="bjack">Nuestros</span><br> CHEFS</h1></a>
+    <div class="col-xs-12 col-md-4 seccion reset-padding" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-nuestroschefs.jpg');">
+      <div class="table">
+        <div class="cell-center">
+          <a href="?page_id=299">
+            <div class="pleca">
+              <div class="table">
+                <div class="cell-center">
+                  <a href="?page_id=469">
+                    <h1 class="bemio"><span class="bjack">Nuestros</span><br> CHEFS</h1>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
-    <div class="col-xs-12 col-md-4 seccion sinPadding" style="background:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');background-size: cover; background-position: center center;">
-      <div class="pleca conPleca">
-        <h1 class="bemio"> <a href="?page_id=469"> NOTICIAS</a></h1>
+    <div class="col-xs-12 col-md-4 seccion reset-padding" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');">
+      <div class="table">
+        <div class="cell-center">
+          <a href="?page_id=469">
+            <div class="pleca">
+              <div class="table">
+                <div class="cell-center">
+                  <h1 class="bemio"><a href="?page_id=469">NOTICIAS</a></h1>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   </div>
