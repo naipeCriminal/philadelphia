ce86e6e594d18bfb6a8b1845f18179cb7351477b
catorcedias <gustavo@catorcedias.com>
2015-04-15 12:50:35 -0500

    Commit Gus

--- wp-content/themes/philadelphia/pages/queEs.php
+++ wp-content/themes/philadelphia/pages/queEs.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="safe-container">
   <div class="col-md-12 text-center">
     <h1 class="bemio">CENTRO GASTRONÓMICO MONDELEZ</h1>
@@ -68,75 +67,4 @@
       </div>
     </div>
   </div>
-=======
-<div class="safe-container">
-  <div class="col-md-12 text-center">
-    <h1 class="bemio">CENTRO GASTRONÓMICO MONDELEZ</h1>
-  </div>
-  <div class="row queEs">
-    <div class="col-xs-12 col-md-8 seccion text-center" style="background:#00adef;">
-      <h1 class="bemio">¿Qué es el <br><span class="bjack">centro gastronómico?</span></h1>
-      <p class="text-left">En Mondelēz nos preocupamos por el crecimiento y la competitividad del mercado de Food Service. Es por eso que desarrollamos el Centro Gastronómico Mondelēz.</p>
-      <p class="text-left">Nuestro Centro Gastronómico cuenta con un equipo profesional, dedicado a combinar sabores, colores, olores y texturas para lograr creaciones culinatias con Queso Crema Philadelphia.</p>
-      <p class="text-left">Uno de nuestros objetivos principales es potencializar tu negocio; sin importar giro o tamaño, te apoyamos con la generación de ideas culinarias innovadoras basadas en las estaciones o tendencias, y te brindamos capacitación de procesos específicos durante todo el año.</p>
-      <h2><span class="bjack">¡Dale más sabor a tu negocio con Philadelphia!</span></h2>
-    </div>
-    <div class="col-xs-12 col-md-4 seccion" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');">
-      <br>
-    </div>
-    <div class="col-xs-12 col-md-6 seccion text-center" style="background:#003a60;">
-      <h1 class="bemio">¿que hacemos <br> para tu negocio?</h1>
-      <p class="text-left">Brindamos soporte en el desarrollo de recetas exclusivas de acuerdo a las necesidades de tu negocio, con la calidad y respaldo del gran reconocimiento de Philadelphia.
-        Te ofrecemos nuestras instalaciones para tener sesiones exclusivas de degustación con las recetas desarrolladas para tu negocio.
-        Damos capacitación en punto de venta sobre las recetas que creamos para ti.
-        Dale play al video y conoce más sobre nuestro Centro Gastronómico.</p>
-      <h1 class="bjack" style="font-size:24px">¡Haz tu negocio más versátil! <br>¡Personaliza tus recetas con Philadelphia!</h1>
-      <p>Escríbenos a: contactofoodservice@mdlz.com</p>
-    </div>
-    <div class="col-xs-12 col-md-6 seccion" style="padding:0px;">
-      <iframe width="100%" height="350" src="https://www.youtube.com/embed/bTtHL4W8G68" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="col-xs-12 col-md-4 seccion" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico_chefs-ubicacion.jpg');">
-      <div class="table">
-        <div class="cell-center">
-          <a href="?page_id=304">
-            <h1 class="bemio">UBICACIÓN</h1>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-xs-12 col-md-4 seccion reset-padding" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-nuestroschefs.jpg');">
-      <div class="table">
-        <div class="cell-center">
-          <a href="?page_id=299">
-            <div class="pleca">
-              <div class="table">
-                <div class="cell-center">
-                  <a href="?page_id=299">
-                    <h1 class="bemio"><span class="bjack">Nuestros</span><br> CHEFS</h1>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="col-xs-12 col-md-4 seccion reset-padding" style="background-image:url('wp-content/themes/philadelphia/assets/img/centro_gastronomico-noticias.jpg');">
-      <div class="table">
-        <div class="cell-center">
-          <a href="?page_id=469">
-            <div class="pleca">
-              <div class="table">
-                <div class="cell-center">
-                  <h1 class="bemio"><a href="?page_id=469">NOTICIAS</a></h1>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
->>>>>>> origin/master
 </div>
